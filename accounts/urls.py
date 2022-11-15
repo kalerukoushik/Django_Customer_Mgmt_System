2deddfdd65ace47b8102f24a319ddf17e2604207
Rick <rick@gmail.com>
2022-11-15 23:45:46 +0530

    Added Project

---
+++ accounts/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
+    path('logout', views.logout_user, name='logout'),
+    path('user/', views.user_page, name='user-page'),
+    path('account/', views.account_settings, name='account'),
+
+    path('', views.home, name='home'),
+    path('products/', views.product, name='products'),
+    path('customers/<str:pk>', views.customer, name='customer'),
+    path('create_order/<str:pk>/', views.create_order, name='create_order'),
+    path('update_order/<str:pk>/', views.update_order, name='update_order'),
+    path('delete_order/<str:pk>/', views.delete_order, name='delete_order'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
